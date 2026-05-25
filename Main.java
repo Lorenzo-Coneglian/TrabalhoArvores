222ac6f3cfa45dd74b82ada89d5cc05f107355bb
Lorenzo-Coneglian <lorenzoconeglian1912@gmail.com>
2026-05-25 07:57:59 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,15 @@
-
+public class Main {
+    public static void main(String[] args) {
+        ArvoreBinaria arvoreBinaria = new ArvoreBinaria();
+        arvoreBinaria.inserir(10);
+        arvoreBinaria.inserir(5);
+        arvoreBinaria.inserir(1);
+        arvoreBinaria.inserir(7);
+        arvoreBinaria.inserir(13);
+        arvoreBinaria.inserir(12);
+        arvoreBinaria.inserir(14);
+        System.out.println(arvoreBinaria.buscarPai(10));
+        arvoreBinaria.remover(10);
+        arvoreBinaria.percurso("Pre");
+    }
+}
